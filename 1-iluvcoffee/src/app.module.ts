46f87217e57a44566ab3170c983cf3f8c8be2934
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-22 23:20:09 +0200

    [ADDITION] TypeORM model introduced and necessary codes has been added.
    The newly added TypeOrmModule.forRoot() code configures database connection:
    
      - type: 'postgres' - Uses PostgreSQL database
      - host/port/username/password - Connection details matching your Docker container
      - autoLoadEntities: true - Automatically finds and loads entity classes
      - synchronize: true - Auto-creates database tables from entities (dev only!)

--- 1-iluvcoffee/src/app.module.ts
+++ 1-iluvcoffee/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
